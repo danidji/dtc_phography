459f89f452d8bf7e34f74436ea20ad019cd845c2
danGbe <73373221+danidji@users.noreply.github.com>
2022-04-23 17:50:20 +0200

    style - handle responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 
 import Navbar from './Navbar'
+import useWindowDimensions from '../hooks/use-window-dimension'
 
 const Header = (): JSX.Element => {
+  const { width } = useWindowDimensions();
 
 
   return (
     <div className="header_wrapper">
-      <div className="logo_wrapper">
-        <Image
-          src="/assets/logo/logo.JPG"
-          // layout='fill'
-          width="200rem"
-          height="200rem"
-        />
-      </div>
-      <Navbar />
+      {(width && width > 768) && (
+        <>
+          <div className="logo_wrapper">
+            <Image
+              src="/assets/logo/logo.JPG"
+              // layout='fill'
+              width="200rem"
+              height="200rem"
+            />
+          </div>
+          < Navbar />
+        </>
+      )}
+      <div>HEADER</div>
     </div>
   )
 }
