0b714bab82e236faaa6f3aff27b1b81ec5de7fa6
danGbe <73373221+danidji@users.noreply.github.com>
2022-04-23 09:51:03 +0200

    feat - init pages prices and contact

---
+++ pages/contact.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+
+const Contact: NextPage = () => {
+  return (
+    <div >
+      <Head>
+        <title>Contact</title>
+        <meta name="description" content="Contact - DTC photo" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
+      <h1 >
+        Me contacter
+      </h1>
+
+    </div>
+  )
+}
+
+export default Contact
+
---
+++ pages/tarifs.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+
+const Prices: NextPage = () => {
+  return (
+    <div >
+      <Head>
+        <title>Tarifs</title>
+        <meta name="description" content="Tarifs - DTC photo" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
+      <h1 >
+        Voici mes prix
+      </h1>
+
+    </div>
+  )
+}
+
+export default Prices
+
