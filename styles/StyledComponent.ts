c4467368bce518dd0df501b36fe12691beecf401
danGbe <73373221+danidji@users.noreply.github.com>
2022-04-28 14:35:35 +0200

    style - create global styled component

---
+++ styles/StyledComponent.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+import { ThemePropsType } from '../src/interfaces'
+
+export const StyledButton = styled.button<ThemePropsType>`
+  background-color: ${p => p.bgColor};
+  padding:1.5rem ;
+  font-size: 1.5rem ;
+  font-weight:bold ;
+  color: #fff;
+  border: none;
+  border-radius: .7rem ;
+  text-transform:uppercase ;
+  cursor: pointer;
+  margin: 2rem 0;
+  
+`
+
+export const StyledP = styled.p<ThemePropsType>`
+  font-size: 1rem ;
+  text-align: center ;
+  color: ${p => p.color};
+  
+  @media (min-width: 768px) {
+    font-size: 1.2rem;
+  }
+  `
