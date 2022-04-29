e44ac99996e7859365e8624cd61a07af4be64023
danGbe <73373221+danidji@users.noreply.github.com>
2022-04-29 14:22:52 +0200

    feat - add Toaster component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Toaster } from 'react-hot-toast'
 
 import Layout from "../src/components/Layout"
 import { ThemeContextProvider } from '../src/state/theme.context'
@@ -9,6 +10,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeContextProvider>
       <Layout>
+        <Toaster
+          position="top-center"
+          toastOptions={{
+            duration: 5000
+          }}
+        />
         <Component {...pageProps} />
       </Layout>
     </ThemeContextProvider>)
