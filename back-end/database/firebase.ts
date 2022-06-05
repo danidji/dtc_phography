6f102615a010cbebbf99aa3cba4b0b49bf56d4ca
danGbe <73373221+danidji@users.noreply.github.com>
2022-06-05 23:20:03 +0200

    feat: define service account key on env variable

--- back-end/database/firebase.ts
+++ back-end/database/firebase.ts
@@ -1,17 +1,16 @@
 import firebase from "firebase-admin"
-import serviceAccount from "../files/lunysse-photographe-firebase-adminsdk-q7iz0-a36ccacd71.json"
 
 const params = {
-  type: serviceAccount.type,
-  projectId: serviceAccount.project_id,
-  privateKeyId: serviceAccount.private_key_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-  clientId: serviceAccount.client_id,
-  authUri: serviceAccount.auth_uri,
-  tokenUri: serviceAccount.token_uri,
-  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-  clientC509CertUrl: serviceAccount.client_x509_cert_url,
+  type: process.env.SERVICE_ACCOUNT_TYPE,
+  projectId: process.env.SERVICE_ACCOUNT_PROJECT_ID,
+  privateKeyId: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+  privateKey: process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
+  clientEmail: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+  clientId: process.env.SERVICE_ACCOUNT_CLIENT_ID,
+  authUri: process.env.SERVICE_ACCOUNT_AUTH_URI,
+  tokenUri: process.env.SERVICE_ACCOUNT_TOKEN_URI,
+  authProviderX509CertUrl: process.env.SERVICE_ACCOUNT_AUTH_PROVIDER,
+  clientC509CertUrl: process.env.SERVICE_ACCOUNT_CLIENT_CERT_URL,
 };
 
 if (!firebase.apps.length) {
