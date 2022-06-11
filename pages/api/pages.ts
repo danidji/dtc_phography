dad2a09c639c6376274d19a0c1916584a4c86a42
danGbe <73373221+danidji@users.noreply.github.com>
2022-06-11 13:20:49 +0200

    feat: add cors on web api

--- pages/api/pages.ts
+++ pages/api/pages.ts
@@ -1,6 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import Cors from 'cors'
+import initMiddleware from '../../back-end/services/init.middleware';
 
 import { processGetPage } from '../../back-end/controllers/pages.controller';
+
+const cors = initMiddleware(Cors({ methods: ['GET'] }))
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,6 +12,7 @@ export default async function handler(
 ) {
 
   if (req.method === "GET") {
+    await cors(req, res)
     await processGetPage(req, res)
   }
 }
