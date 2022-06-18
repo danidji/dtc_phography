2b3df14e4fbf52b067ed26ef85e08f562802e075
danGbe <73373221+danidji@users.noreply.github.com>
2022-06-18 22:03:17 +0200

    test: change url api

--- src/repositories/contact.repo.ts
+++ src/repositories/contact.repo.ts
@@ -5,7 +5,9 @@ import { ContactFormType } from '../interfaces/contact.interfaces';
 export const instance = axios.create({
   baseURL: process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api"
-    : "https://62a47c7553f2f10a6b9344a2--iridescent-snickerdoodle-82bec4.netlify.app/",
+    // : "http://localhost:3000/api",
+    : "https://62a47c7553f2f10a6b9344a2--iridescent-snickerdoodle-82bec4.netlify.app/api",
+  // : "https://62a47c7553f2f10a6b9344a2--iridescent-snickerdoodle-82bec4.netlify.app/",
   headers: {
     "Accept": "*/*",
     "Content-Type": "application/json",
