299cc67b9e46ce848db93167a88fcc07b9c7ad0e
danGbe <73373221+danidji@users.noreply.github.com>
2022-06-18 22:07:34 +0200

    test: change url api

--- src/repositories/contact.repo.ts
+++ src/repositories/contact.repo.ts
@@ -6,7 +6,7 @@ export const instance = axios.create({
   baseURL: process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api"
     // : "http://localhost:3000/api",
-    : "https://62a47c7553f2f10a6b9344a2--iridescent-snickerdoodle-82bec4.netlify.app/api",
+    : "https://main--iridescent-snickerdoodle-82bec4.netlify.app/api",
   // : "https://62a47c7553f2f10a6b9344a2--iridescent-snickerdoodle-82bec4.netlify.app/",
   headers: {
     "Accept": "*/*",
