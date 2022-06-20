7390e259f3091ee538bc2df985d58f2e9c910ba1
danGbe <73373221+danidji@users.noreply.github.com>
2022-06-20 17:51:26 +0200

    test: change url api

--- src/repositories/pages.repo.ts
+++ src/repositories/pages.repo.ts
@@ -2,6 +2,6 @@ import { instance } from "./contact.repo";
 import { PageContentType } from "../../back-end/models/pages.interface";
 
 export const axiosGetPageContent = async (id: string): Promise<PageContentType> => {
-  const response = await instance.get(`/pages/?id=${id}`)
+  const response = await instance.get(`/pages?id=${id}`)
   return response.data
 }
