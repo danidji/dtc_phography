8b3bf8bf8760cfb6dd40e0bd0704f004c5210702
danGbe <73373221+danidji@users.noreply.github.com>
2022-06-20 22:56:25 +0200

    refacto: delete comment

--- src/repositories/axios.instance.ts
+++ src/repositories/axios.instance.ts
@@ -5,7 +5,6 @@ export const instance = axios.create({
     ? "http://localhost:3000/api"
     // : "http://localhost:3000/api",
     : "https://www.lunysse-photographe.com/api",
-  // : "https://62a47c7553f2f10a6b9344a2--iridescent-snickerdoodle-82bec4.netlify.app/",
   headers: {
     "Accept": "*/*",
     "Content-Type": "application/json",
